7102b94c5252414043267ea54ad75232e84d3225
terrance <webmaster@b-stats.org>
2014-10-27 03:16:04 -0400

    This class may come in handy.

---
+++ inc/classes/Archivers.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Archivers class
+ * Holds static functions and constants relating to the starting/stopping
+ * of archivers.
+ * 
+ * Note: Does not currently work with the generic "archiver.php" script.
+ * (At present all archive scripts are slightly modified for each board)
+ */
+class Archivers {
+    const STOPPED_UNCLEAN = -1;
+    const STOPPED = 0;
+    const RUNNING = 1;
+    const STOPPING = 2;
+    
+    static function getStatus($board){
+        if(file_exists(Site::getPath()."/backend/$board.pid")){
+            $pid = file_get_contents(Site::getPath()."/backend/$board.pid");
+            if(PHP_OS == "Linux"){
+                if(file_exists("/proc/$pid")){
+                    if(file_exists(Site::getPath() . "/backend/$board.kill")) {
+                        return self::STOPPING;
+                    } else {
+                        return self::RUNNING;
+                    }
+                }
+                else{
+                    return self::STOPPED_UNCLEAN;
+                }
+            }
+            else{ //Windows
+                $running = false;
+                $processes = explode("\n", shell_exec("tasklist.exe"));
+                foreach ($processes as $process) {
+                    if (preg_match('/^(.*)\s+'.$pid.'/', $process)){
+                        $running = true;
+                        break;
+                    }
+                }
+                if($running = false){
+                    return self::STOPPED_UNCLEAN;
+                }
+                else{
+                    return self::RUNNING;
+                }
+            }
+        }
+        return self::STOPPED;
+    }
+    
+    static function run($board){
+        if(file_exists(Site::getPath()."/backend/$board-archiver.php")){
+            if(self::getStatus($board) == self::STOPPED || self::getStatus($board) == self::STOPPED_UNCLEAN){
+                exec("cd ".Site::getPath()."/backend/ && screen -dmS $board php $board-archiver.php");
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    static function stop($board){
+        if(self::getStatus($board) == self::RUNNING){
+            touch(Site::getPath()."/backend/$board.kill");
+        }
+    }
+}
