2ce7aeb89ffe2f0793c78d39d6fadec6e2e1608e
terrance <webmaster@b-stats.org>
2014-10-28 02:23:35 -0400

    New API functions: boardInfo, allBoardsInfo, postLinks

--- api.php
+++ api.php
@@ -1,10 +1,9 @@
 <?php
-
 include 'inc/config.php';
-switch($_POST['a']){
+switch($_REQUEST['a']){
     case 'post':
-        $board = isset($_POST['b']) ? $_POST['b'] : 'b';
-        $no = $_POST['id'];
+        $board = isset($_REQUEST['b']) ? $_REQUEST['b'] : 'b';
+        $no = $_REQUEST['id'];
         try{
             $post = new Post(Model::getPost($board, $no)->fetch_assoc(),$board);
             echo str_replace('data-original','src',$post->display());
@@ -13,4 +12,25 @@ switch($_POST['a']){
             echo 'Error: '.$e->getMessage();
         }
         break;
+    case 'boardInfo':
+        if(!isset($_REQUEST['b'])) exit;
+        header("Content-type: application/json");
+        try{
+            $board = new Board($_REQUEST['b']);
+            echo json_encode($board->getBoardInfo());
+        }
+        catch(Exception $ex){
+            echo json_encode(array("error"=>"Could not load board info"));
+        }
+        break;
+    case 'allBoardsInfo':
+        header("Content-type: application/json");
+        echo json_encode(Model::getBoards());
+        break;
+    case 'postLinks':
+        if(!isset($_REQUEST['b'])) exit;
+        $board = $_REQUEST['b'];
+        $post = (int)$_REQUEST['p'];
+        header("Content-type: application/json");
+        echo json_encode(["posts"=>Model::getPostsWithLinkToPost($board, $post),"time"=>microtime(true)-CONFIG_INC_TIME]);
 }
